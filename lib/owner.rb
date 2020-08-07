7896bce5bed1f4d73d7cf0d64a658b7b3ffdbf54
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-07 01:04:04 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -54,7 +54,7 @@ attr_reader  :name, :species
   
   def sell_pets
     #binding.pry
-    feed_cats.reverse
+   all.each {|m| m.mood = "nervous"}
   end
   #can sell all its pets, which makes them nervous (FAILED - 1)
   
